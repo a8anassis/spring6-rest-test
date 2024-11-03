41b1ba5d269a6332d31e63645ab850aa391cbd5b
Th. Androutsos <a8anassis@gmail.com>
2024-11-03 14:37:34 +0200

    updates

--- src/main/java/gr/aueb/cf/datatest/repository/EducationalUnitRepository.java
+++ src/main/java/gr/aueb/cf/datatest/repository/EducationalUnitRepository.java
@@ -1,7 +1,6 @@
 package gr.aueb.cf.datatest.repository;
 
 import gr.aueb.cf.datatest.model.static_data.EducationalUnit;
-import gr.aueb.cf.datatest.model.static_data.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EducationalUnitRepository extends JpaRepository<EducationalUnit, Long> {
